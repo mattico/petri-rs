4b355f5390986d79527a5d9606b687606f18ed43
Matt Ickstadt <mattico8@gmail.com>
2016-04-25 15:19:01 -0500

    More webGl stuff

--- src/www/js/fragment.glsl
+++ src/www/js/fragment.glsl
@@ -1,3 +1,5 @@
-void main() {
-  gl_FragColor = vec4(0,1,0,1);  // green
+precision mediump float;
+
+void main(void) {
+    gl_FragColor = vec4(1.0, 0.5, 0.0, 1.0);
 }
--- src/www/js/main.js
+++ src/www/js/main.js
@@ -1,90 +1,185 @@
 'use strict';
 
-var game = {};
+var game = {
+    lastTime: 0,
+    players: [],
+    gl: null,
+    program: null,
 
-game.init = function() {
-    window.onresize = onResize;
-    
-    var canvas = document.getElementById('canvas');
-    
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    
-    if (canvas.getContext) {
-        
-        var dp = window.devicePixelRatio;
-        
-        var gl = canvas.getContext('webgl', {
-            alpha: false,
-            depth: false,
-            stencil: false,
-            antialias: true,
+    init: function() {
+        window.onresize = onResize;
+
+        onResize();
+
+        if (canvas.getContext) {
+
+            var gl = canvas.getContext('webgl', {
+                alpha: false,
+                depth: false,
+                stencil: false,
+                antialias: true,
+            });
+
+            var vertex = getShader(gl, 'js/vertex.glsl', gl.VERTEX_SHADER);
+            var fragment = getShader(gl, 'js/fragment.glsl', gl.FRAGMENT_SHADER);
+
+            var program = gl.createProgram();
+            gl.attachShader(program, vertex);
+            gl.attachShader(program, fragment);
+            gl.linkProgram(program);
+
+            if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+                var info = gl.getProgramInfoLog(program);
+                throw 'Could not link WebGL shader. \n\n' + info;
+            }
+
+            gl.useProgram(program);
+
+            this.gl = gl;
+            this.program = program;
+
+            this.players.push(new Player(0, 'Matt', 'green', new Point(0, 0), 100));
+
+            this.lastTime = performance.now();
+
+            this.update(performance.now());
+
+        } else {
+            throw 'No canvas support!';
+        }
+    },
+
+    update: function(time) {
+        var dt = this.lastTime - time;
+        this.lastTime = time;
+
+        var gl = this.gl;
+        var program = this.program;
+
+        this.players.forEach(function(v) {
+           v.update(dt);
+           v.draw(gl, program); 
         });
 
-        var vertex = getShader(gl, 'js/vertex.glsl', gl.VERTEX_SHADER);
-        var fragment = getShader(gl, 'js/fragment.glsl', gl.FRAGMENT_SHADER);
+        window.requestAnimationFrame(this.update.bind(this));
+    }
+};
 
-        var program = gl.createProgram();
-        gl.attachShader(program, vertex);
-        gl.attachShader(program, fragment);
-        gl.linkProgram(program);
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+    this.add = function(point) {
+        return Point(this.x + point.x,
+                     this.y + point.y);
+    };
+    this.sub = function(point) {
+        return Point(this.x - point.x,
+                     this.y - point.y);
+    };
+    this.distance = function(point) {
+        return Math.sqrt(Math.pow(this.x - point.x, 2) +
+                         Math.pow(this.y - point.y, 2));
+    };
+    this.clone = function() {
+        return new Point(this.x, this.y);
+    };
+    this.as_array = function() {
+        return [x, y];
+    };
+}
 
-        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-            var info = gl.getProgramInfoLog(program);
-            throw "Could not link WebGL shader. \n\n" + info;
-        }
+function Player(id, name, color, position, size) {
+    this.id = id;
+    this.name = name;
+    this.color = color;
+    this.position = position;
+    this.size = size;
+    this.velocity = new Point(0, 0);
+    this.blobs = [new Blob(this, size, position)];
 
-        gl.useProgram(program);
-        
-        // look up where the vertex data needs to go.
-        var positionLocation = gl.getAttribLocation(program, "a_position");
+    this.split = function() {
+        b = [];
+        this.blobs.forEach(function(v) {
+            b.push.apply(v.split());
+        });
+        this.blobs = b;
+    };
 
-        // Create a buffer and put a single clipspace rectangle in
-        // it (2 triangles)
-        var buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-        gl.bufferData(
-            gl.ARRAY_BUFFER, 
-            new Float32Array([
-                -1.0, -1.0, 
-                 1.0, -1.0, 
-                -1.0,  1.0, 
-                -1.0,  1.0, 
-                 1.0, -1.0, 
-                 1.0,  1.0]), 
-            gl.STATIC_DRAW);
-        gl.enableVertexAttribArray(positionLocation);
-        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+    this.update = function(dt) {
+        this.blobs.forEach(function(v) {
+            v.update(dt);
+        })
+    }
 
-        // draw
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-        game.draw(gl, 0);
-        
-    } else {
-        console.log('No canvas support!');
+    this.draw = function(gl, program) {
+        this.blobs.forEach(function(v) {
+            v.draw(gl, program);
+        })
     }
 }
 
-game.update = function(gl, time) {
+function Blob(player, size, position, velocity) {
+    this.player = player;
+    this.size = size;
+    this.position = position;
+    this.velocity = velocity || new Point(0, 0);
 
+    this.radius = circleAreaToRadius(this.size);
+
+    this.split = function() {
+        return [new Blob(this.player, this.size / 2, this.position, this.velocity),
+                new Blob(this.player, this.size / 2, this.position, this.velocity)];
+    };
+
+    this.update = function(dt) {
+        this.radius = circleAreaToRadius(this.size);
+    };
+
+    this.draw = function(gl, program) {
+        const NumVertices = 16;
+        const FanAngle = (2 * Math.PI) / NumVertices;
+        var vertices = [this.position.clone()];
+
+        for (var i = 1; i <= NumVertices; i++) {
+            var theta = i * FanAngle;
+            var x = this.position.x + Math.cos(theta) * this.radius;
+            var y = this.position.y + Math.cos(theta) * this.radius;
+            vertices.push(new Point(x, y));
+        }
+
+        var vertexData = new Float32Array(flattenPointArray(vertices));
+
+        var buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, vertexData, gl.STATIC_DRAW);
+
+        var resolutionLocation = gl.getUniformLocation(program, "u_resolution");
+        gl.uniform2f(resolutionLocation, canvas.width, canvas.height);
+
+        gl.enableVertexAttribArray(positionLocation);
+
+        var positionLocation = gl.getAttribLocation(program, "a_position");
+        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 2 * Float32Array.BYTES_PER_ELEMENT, 0);
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, vertexData.length/2);
+
+    };
 }
 
-game.draw = function(gl, time) {
-
-
-
-
-    window.requestAnimationFrame(function(time) { game.draw(gl, time) });
+// Event Handlers
+function onLoad() {
+    game.init();
 }
 
 function onResize() {
     var canvas = document.getElementById('canvas');
+    var dp = window.devicePixelRatio;
     
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth * dp;
+    canvas.height = window.innerHeight * dp;
 }
 
+
+// Utilities
 function getShader(gl, path, type) {
     var request = new XMLHttpRequest();
     request.open('GET', path, false);
@@ -97,8 +192,20 @@ function getShader(gl, path, type) {
 
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
         var info = gl.getShaderInfoLog(shader);
-        throw "Could not compile WebGL shader. \n\n" + info;
+        throw 'Could not compile WebGL shader. \n\n' + info;
     }
 
     return shader;
+}
+
+function circleAreaToRadius(area) {
+    return Math.sqrt(area / Math.PI);
+}
+
+function flattenPointArray(array) {
+    var result = [];
+    array.forEach(function(v) {
+       result = result.concat(v.as_array()); 
+    });
+    return result;
 }
