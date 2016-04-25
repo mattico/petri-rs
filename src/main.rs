23a8bf9450676f074c91a67e1254ee6e74bc0f04
Matt Ickstadt <mattico8@gmail.com>
2016-04-25 13:05:56 -0500

    Add web scaffolding

---
+++ src/main.rs
@@ -0,0 +1,60 @@
+extern crate iron;
+#[macro_use] extern crate log;
+extern crate env_logger;
+extern crate mount;
+extern crate router;
+extern crate staticfile;
+extern crate time;
+extern crate websocket;
+
+use std::path::Path;
+use std::time::Duration;
+
+use iron::prelude::*;
+use iron::{BeforeMiddleware, AfterMiddleware, typemap};
+use mount::Mount;
+use router::Router;
+use staticfile::Static;
+use time::precise_time_ns;
+
+struct ResponseTime;
+
+impl typemap::Key for ResponseTime { type Value = u64; }
+
+impl BeforeMiddleware for ResponseTime {
+    fn before(&self, req: &mut Request) -> IronResult<()> {
+        req.extensions.insert::<ResponseTime>(precise_time_ns());
+        println!("Requested url: {}", req.url.path.join("/"));
+        Ok(())
+    }
+}
+
+impl AfterMiddleware for ResponseTime {
+    fn after(&self, req: &mut Request, res: Response) -> IronResult<Response> {
+        let delta = precise_time_ns() - *req.extensions.get::<ResponseTime>().unwrap();
+        println!("Request took: {} ms", (delta as f64) / 1000000.0);
+        Ok(res)
+    }
+}
+
+
+fn main() {
+    
+    env_logger::init().unwrap();
+    
+    // let cache_duration = Duration::from_secs(60*60*24*30);
+    let cache_duration = Duration::from_secs(0);
+    
+    let mut mount = Mount::new();
+    mount.mount("/", Static::new(Path::new("src/www")).cache(cache_duration));
+    mount.mount("/favicon.ico", Static::new(Path::new("src/www/img/favicon.ico")).cache(cache_duration));
+    
+    let mut chain = Chain::new(mount);
+    chain.link_before(ResponseTime);
+    chain.link_after(ResponseTime);
+    
+    println!("Starting in '{}'", std::env::current_dir().unwrap().to_str().unwrap());
+    
+    println!("Serving on http://localhost:3000");
+    Iron::new(chain).http("localhost:3000").unwrap(); 
+}
---
+++ src/www/js/main.js
@@ -0,0 +1,98 @@
+'use strict';
+
+function init() {
+    window.onresize = onResize;
+    
+    var canvas = document.getElementById('canvas');
+    
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    
+    if (canvas.getContext) {
+        
+        var dp = window.devicePixelRatio;
+        
+        var gl = canvas.getContext('webgl', {
+            alpha: false,
+            depth: false,
+            stencil: false,
+            antialias: true,
+        });
+
+        var vertex = getShader(gl, 'js/vertex.glsl', gl.VERTEX_SHADER);
+        var fragment = getShader(gl, 'js/fragment.glsl', gl.FRAGMENT_SHADER);
+
+        var program = gl.createProgram();
+        gl.attachShader(program, vertex);
+        gl.attachShader(program, fragment);
+        gl.linkProgram(program);
+
+        if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+            var info = gl.getProgramInfoLog(program);
+            throw "Could not link WebGL shader. \n\n" + info;
+        }
+
+        gl.useProgram(program);
+        
+        // look up where the vertex data needs to go.
+        var positionLocation = gl.getAttribLocation(program, "a_position");
+
+        // Create a buffer and put a single clipspace rectangle in
+        // it (2 triangles)
+        var buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+        gl.bufferData(
+            gl.ARRAY_BUFFER, 
+            new Float32Array([
+                -1.0, -1.0, 
+                 1.0, -1.0, 
+                -1.0,  1.0, 
+                -1.0,  1.0, 
+                 1.0, -1.0, 
+                 1.0,  1.0]), 
+            gl.STATIC_DRAW);
+        gl.enableVertexAttribArray(positionLocation);
+        gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
+
+        // draw
+        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        
+        draw(gl, 0);
+        
+    } else {
+        console.log('No canvas support!');
+    }
+}
+
+function getShader(gl, path, type) {
+    var request = new XMLHttpRequest();
+    request.open('GET', path, false);
+    request.send();
+    
+    var shader = gl.createShader(type);
+    
+    gl.shaderSource(shader, request.responseText);
+    gl.compileShader(shader);
+
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+        var info = gl.getShaderInfoLog(shader);
+        throw "Could not compile WebGL shader. \n\n" + info;
+    }
+
+    return shader;
+}
+
+function draw(gl, time) {
+
+
+
+
+    window.requestAnimationFrame(function(time) { draw(gl, time) });
+}
+
+function onResize() {
+    var canvas = document.getElementById('canvas');
+    
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
